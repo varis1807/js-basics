dc0ffaafb15eeaaa79c4999404fdbc2db3fa66e6
varis <vrvarisrana123@gmail.com>
2021-07-16 10:22:35 +0530

    first commit

---
+++ js_basics.js
@@ -0,0 +1,115 @@
+// var x=5;
+// var y=5;
+// var z = x+y;
+// console.log(z);
+
+// var a="car";
+// var b="Varis rana is best coder in whole india!!!";
+// console.log(a+" "+b);
+
+// var x=1+2+3+4;
+// console.log(x);
+
+// var a="5"+1+2;
+// console.log(a);
+
+// let and var variable
+
+// Global Scope
+// var carName="volvo";
+// function muFunc(){
+//     console.log(carName);
+// }
+// muFunc();
+
+// Function Scope
+// function my(){
+//     var a = "#varis is best coder";
+//     console.log(a);
+// }
+// my();
+
+// function my(){
+//     let a = "#varis is best coder";
+//     console.log(a);
+// }
+// my();
+
+// let i=5;
+// for(let i=1; i<=10; i++){
+//     console.log(i);
+// }
+// console.log(i);
+
+// let a=10;
+// a =29;
+// console.log(a); // reassingnable  //no redeclarable
+
+// var a=10;
+// var a=20;
+// console.log(a); // reassingnable  // redeclarable
+// var a=2;
+// let b=3;
+// {
+//     var a=4;
+//     let b=7;
+//     console.log(a);
+//     console.log(b);
+// }
+
+// let a = 1;
+// {
+//   let a = 2;
+//   {
+//     let a = 3;
+//     {
+//       let a = 4;
+//       {
+//         let a = 5;
+//         {
+//           let a = 6;
+//           console.log(a);
+//         }
+//         console.log(a);
+//       }
+//       console.log(a);
+//     }
+//     console.log(a);
+//   }
+//   console.log(a);
+// }
+// console.log(a);
+
+
+// JavaScript Functions
+
+// function my(p1, p2){
+//     return p1*p2;
+// }
+// console.log(my(2,2));
+
+// function my(p1, p2){
+//     console.log(p1*p2);
+// }
+// my(2,2)
+
+
+
+// JavaScript Objects
+
+// objects are variable but objects can store or contain many values
+// var person = {
+//     fN:"varis",
+//     lN:"rana",
+//     age:18,
+//     height:5.2,
+//     hoby:"Coding"
+// }
+// person[1]="ranaji";
+// console.log(person.hoby);
+// console.log(Object.keys(person));
+// console.log(Object.values(person));
+// console.log(person["rana"]);
+
+
+
